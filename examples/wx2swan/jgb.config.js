2995c5c97c2ba8312a1134378bb6b835b98f13b9
蒋瑶 <landn172@gmail.com>
2018-10-10 14:19:49 +0800

    test(demo): 添加微信官方demo转百度的example

---
+++ examples/wx2swan/jgb.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  cache: false,
+  presets: ['weapp'],
+  plugins: [['css', {
+    outExt: '.css',
+    extensions: ['.wxss'],
+  }], ['html', {
+    extensions: ['.wxml'],
+    outExt: '.swan'
+  }]]
+}
