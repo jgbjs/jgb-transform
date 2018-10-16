32c1d4d9344d2637777fea19df4312fcee43769f
蒋瑶 <landn172@gmail.com>
2018-10-16 17:06:40 +0800

    feat(babel-plugin): 转换时自动引入适配库默认:miniapp-adapter

--- packages/babel-plugin-transform-miniprogram/src/index.js
+++ packages/babel-plugin-transform-miniprogram/src/index.js
@@ -6,54 +6,126 @@
  * export default wx => export default swan
  */
 
+const resolve = require('resolve');
+const path = require('path')
+
 let SOURCE = 'wx'
 let TARGET = 'swan'
+/* 适配库包名 */
+let adapterLib = '';
+const DEFAULT_LIB = 'miniapp-adapter';
+
+const updateVisitor = {
+  Identifier(path) {
+    if (path.node.name === SOURCE) {
+      const state = this.ctx.file
+      state._isUsedTransform = true;
+      path.node.name = TARGET;
+    }
+  }
+}
+
+const mappingAdapterLib = {
+  wx: 'wechat',
+  swan: 'baidu',
+  my: 'aliapp'
+}
 
 export default function ({types:t}) {
   return {
     pre(state) {
       const [plugin] = state.opts.plugins
       const [, opts={}] = plugin
-      if (opts.SOURCE) {
-        SOURCE = opts.SOURCE
+      if (opts.source) {
+        SOURCE = opts.source
       }
 
-      if (opts.TARGET) {
-        TARGET = opts.TARGET
+      if (opts.target) {
+        TARGET = opts.target
       }
+
+      adapterLib = getAdapterRealPath(opts.lib || DEFAULT_LIB)
+    },
+    post(state) {
+      if (SOURCE === TARGET) return
+      if (state.ast.isImported) return
+      if (!state._isUsedTransform) return
+      if (!adapterLib) return
+      const importAst = t.importDeclaration([t.importDefaultSpecifier(t.identifier(TARGET))], t.stringLiteral(adapterLib))
+      state.ast.isImported = true
+      state.ast.program.body.unshift(importAst)
     },
     visitor: {
       MemberExpression(path) {
-        if(!path.node.object) return
+        if (!path.node.object) return
         if (path.node.object.name === SOURCE) {
-          path.traverse(updateVisitor)
+          path.traverse(updateVisitor, {
+            ctx: this
+          })
         }
       },
       VariableDeclarator(path) {
         if (!path.node.init) return
         if (path.node.init.name === SOURCE) {
-          path.traverse(updateVisitor)
+          path.traverse(updateVisitor, {
+            ctx: this
+          })
         }
       },
       ExportDefaultDeclaration(path) {
-        if(!path.node.declaration) return
+        if (!path.node.declaration) return
         if (path.node.declaration.name === SOURCE) {
-          path.traverse(updateVisitor)
+          path.traverse(updateVisitor, {
+            ctx: this
+          })
         }
       },
       AssignmentExpression(path) {
         if (path.node.right.name === SOURCE) {
-          path.traverse(updateVisitor)
+          path.traverse(updateVisitor, {
+            ctx: this
+          })
         }
       }
     }
   }
 }
 
-const updateVisitor = {
-  Identifier(path) {
-    if (path.node.name === SOURCE) {
-      path.node.name = TARGET;
-    }
+function getAdapterRealPath(requireLib) {
+  if (SOURCE === TARGET) return
+  let lib;
+  try {
+    lib = resolve.sync(requireLib, {
+      basedir: process.cwd()
+    })
+  } catch (error) {
+    console.log(error)
+    return;
   }
+  lib = require(lib);
+  lib = lib.default || lib;
+  if (requireLib === DEFAULT_LIB) {
+    const requireAdapter = lib
+    const adapterRealPath = requireAdapter(mappingAdapterLib[TARGET] || TARGET)
+    if (adapterRealPath) return formatRequirePath(requireLib, adapterRealPath)
+    return
+  }
+  return formatRequirePath(requireLib, lib(TARGET))
+}
+
+function formatRequirePath(requireLib, adapterPath) {
+  if (!path.isAbsolute(adapterPath)) {
+    return adapterPath.replace(/\\/g, "/");
+  }
+
+  if (path.isAbsolute(requireLib)) {
+    return adapterPath.replace(/\\/g, "/")
+  }
+
+  /**
+   *  xxx/miniapp-adapter/index => miniapp-adapter/index
+   */
+  const [libName] = requireLib.split('/')
+  const [, relativePath] = adapterPath.split(libName)
+  return `${libName}${relativePath}`.replace(/\\/g, "/")
 }
--- packages/babel-plugin-transform-miniprogram/test/index.test.js
+++ packages/babel-plugin-transform-miniprogram/test/index.test.js
@@ -2,48 +2,65 @@ import pluginTester from 'babel-plugin-tester'
 import * as path from 'path'
 import plugin from '../src'
 
+const beautify = require('js-beautify').js
+
+function pretty(data) {
+  return beautify(data, {
+    indent_size: 2,
+    space_in_empty_paren: true
+  })
+}
+
 pluginTester({
   pluginName: `babel-plugin-transform-wx-to-swan`,
   plugin,
   pluginOptions: {
-    SOURCE: 'wx',
-    TARGET: 'swan',
+    source: 'wx',
+    target: 'swan',
   },
   // fixtures: path.join(__dirname, 'fixtures'),
   tests: {
     "wx.request({})": {
       code: "wx.request({});",
-      output: "swan.request({});"
+      output: pretty(`import swan from "miniapp-adapter/lib/platform/baidu/index.js";      
+      swan.request({});`)
     },
     [`wx["request"]({})`]: {
       code: `wx["request"]({});`,
-      output: `swan["request"]({});`,
+      output: pretty(`import swan from "miniapp-adapter/lib/platform/baidu/index.js";
+      swan["request"]({});`),
     },
     "var request = wx.request": {
       code: `var request = wx.request;`,
-      output: `var request = swan.request;`
+      output: pretty(`import swan from "miniapp-adapter/lib/platform/baidu/index.js";
+      var request = swan.request;`)
     },
     "var temp = wx": {
       code: `var temp = wx;`,
-      output: `var temp = swan;`
+      output: pretty(`import swan from "miniapp-adapter/lib/platform/baidu/index.js";
+      var temp = swan;`)
     },
     "var systemInfo = wx.getSystemInfoSync();": {
       code: `var systemInfo = wx.getSystemInfoSync();`,
-      output: `var systemInfo = swan.getSystemInfoSync();`
+      output: pretty(`import swan from "miniapp-adapter/lib/platform/baidu/index.js";
+      var systemInfo = swan.getSystemInfoSync();`)
     },
     [`export default wx`]: {
       code: `export default wx`,
-      output: `export default swan;`
+      output: pretty(`import swan from "miniapp-adapter/lib/platform/baidu/index.js";
+      export default swan;`)
     },
     [`module.exports = wx;`]: {
       code: `module.exports = wx;`,
-      output: `module.exports = swan;`
+      output: pretty(`import swan from "miniapp-adapter/lib/platform/baidu/index.js";
+      module.exports = swan;`)
     },
     "ali: var request = wx.request": {
       code: `var request = wx.request;`,
-      output: `var request = my.request;`,
+      output: pretty(`import my from "miniapp-adapter/lib/platform/aliapp/index.js";
+      var request = my.request;`),
       pluginOptions: {
-        TARGET: 'my',
+        target: 'my',
       },
     }
   },
