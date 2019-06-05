fc77c8b4233ba6b9efe684b6de71d6fa04bd0888
蒋瑶 <landn172@gmail.com>
2019-06-05 17:40:46 +0800

    fix(miniapp-adapter): remove api proxy

--- packages/miniapp-adapter/src/utils/index.js
+++ packages/miniapp-adapter/src/utils/index.js
@@ -1,25 +1,25 @@
 export function ProxyInvokePlatformApi(api = {}) {
-  if (typeof Proxy !== "undefined") {
-    const proxyApi = new Proxy(
-      {},
-      {
-        get(target, key, receiver) {
-          if (api[key]) return api[key];
+  // if (typeof Proxy !== "undefined") {
+  //   const proxyApi = new Proxy(
+  //     {},
+  //     {
+  //       get(target, key, receiver) {
+  //         if (api[key]) return api[key];
 
-          // almost all wx[key] is a function
-          return () => {
-            console.warn(`invoke wx.${key}, but not achieve function`);
-          };
-        },
-        set(target, key, value) {
-          api[key] = value;
-          return value;
-        }
-      }
-    );
+  //         // almost all wx[key] is a function
+  //         return () => {
+  //           console.warn(`invoke wx.${key}, but not achieve function`);
+  //         };
+  //       },
+  //       set(target, key, value) {
+  //         api[key] = value;
+  //         return value;
+  //       }
+  //     }
+  //   );
 
-    return proxyApi
-  }
+  //   return proxyApi
+  // }
   return api;
 }
 
