e090ed616ec601731caed62ea4b258165e047751
sunyubo <sunyubo34@gmail.com>
2020-05-22 18:03:15 +0800

    fix: 修复支付宝不传confirmText和cancelText时把undefined展示出来的问题

--- packages/miniapp-adapter/src/platform/aliapp/interface/showModal.js
+++ packages/miniapp-adapter/src/platform/aliapp/interface/showModal.js
@@ -3,8 +3,8 @@ import { ExtendExistValue } from '../../../utils/index'
 export function showModal(opts) {
   const { success } = opts;
   const options = ExtendExistValue(opts, {
-    confirmButtonText: opts.confirmText,
-    cancelButtonText: opts.cancelText,
+    confirmButtonText: opts.confirmText || '确定',
+    cancelButtonText: opts.cancelText || '取消',
     success(res) {
       res.cancel = !res.confirm
       success && success.call(this, res)
