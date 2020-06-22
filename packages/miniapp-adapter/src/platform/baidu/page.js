88530f984af1b5b0b51a9b1bde0723360b22eb8a
蒋瑶 <landn172@gmail.com>
2020-06-22 17:14:24 +0800

    feat: 🎸 补全百度Page缺失的函数

--- packages/miniapp-adapter/src/platform/baidu/page.js
+++ packages/miniapp-adapter/src/platform/baidu/page.js
@@ -1,6 +1,16 @@
+import { defineProperty } from '../../utils/index'
+
 export default function AdapterBaiduPage(opts, ...otherOpts) {
   // 取最后一个参数作为Page
   const InjectPage = otherOpts.length ? (otherOpts[otherOpts.length - 1] || Page) : Page;
+
+  const oldLoad = opts.onLoad || (() => {
+  });
+
+  opts.onLoad = function (...args) {
+    extendInstance.call(this)
+    oldLoad.apply(this, args)
+  }
 
   InjectPage(opts)
 }
@@ -9,4 +19,13 @@ export function WrapPage(InjectPage = Page) {
   return (opts) => {
     AdapterBaiduPage(opts, InjectPage)
   }
+}
+
+
+/**
+ * 扩展实例属性
+ * @param {*} ctx 
+ */
+function extendInstance() {
+  defineProperty(this, 'createIntersectionObserver', (opts) => swan.createIntersectionObserver(this, opts))
 }
