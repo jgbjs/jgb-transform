e45035b78ca73c86f6f4bab06cc84ed28e41829a
蒋瑶 <landn172@gmail.com>
2020-07-03 18:15:09 +0800

    fix: 🐛 remove set alipay default request ContentType

--- packages/miniapp-adapter/src/platform/aliapp/network/request.js
+++ packages/miniapp-adapter/src/platform/aliapp/network/request.js
@@ -1,9 +1,9 @@
 export const request = opts => {
   const { success } = opts
   const innerRequest = my.request || my.httpRequest;
-  // 默认的请求的ContentType为 json
+  // 默认请求的ContentType为 json
   const headers = Object.assign({
-    'Content-Type': 'application/json'
+    // 'Content-Type': 'application/json'
   }, opts.header || opts.headers || {});
 
   return innerRequest({
