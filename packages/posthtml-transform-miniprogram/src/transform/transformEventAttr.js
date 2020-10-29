d9d8663dcd18ec37dfefbb2af1a62278b7e4f86a
landn172 <landn172@gmail.com>
2020-10-29 18:30:34 +0800

    fix: 修复支付宝转义xml遗漏一些事件

--- packages/posthtml-transform-miniprogram/src/transform/transformEventAttr.js
+++ packages/posthtml-transform-miniprogram/src/transform/transformEventAttr.js
@@ -1,55 +1,59 @@
 /**
- * 通用属性事件名转换 
- * @param {*} source 
- * @param {*} target 
+ * 通用属性事件名转换
+ * @param {*} source
+ * @param {*} target
  */
 export function createTransformEventAttr(source, target) {
   // 支付宝小程序和微信小程序差异比较多
-  if (target !== 'aliapp') {
-    return returnSelf
+  if (target !== "aliapp") {
+    return returnSelf;
   }
 
-  return transformAliappEventAttr
+  return transformAliappEventAttr;
 }
 
-const MATCH_EVENT_ATTR = /^(bind|catch){1}:?(.*)/
+const MATCH_EVENT_ATTR = /^(bind|catch){1}:?(.*)/;
 
 /**
  * wx转换支付宝事件
- * @param {*} node 
+ * @param {*} node
  */
 export function transformAliappEventAttr(node) {
-  const attrs = node.attrs
+  const attrs = node.attrs;
   if (!attrs) return node;
   const commonEventMapping = {
-    touchstart: 'TouchStart',
-    touchmove: 'TouchMove',
-    touchend: 'TouchEnd',
-    touchcancel: 'TouchCancel',
-    longtap: 'LongTap'
-  }
+    touchstart: "TouchStart",
+    touchmove: "TouchMove",
+    touchend: "TouchEnd",
+    touchcancel: "TouchCancel",
+    longtap: "LongTap",
+    transitionend: "TransitionEnd",
+    animationstart: "AnimationStart",
+    animationiteration: "AnimationIteration",
+    animationend: "AnimationEnd",
+  };
   // capture-bind:* , capture-catch:* 支付宝不支持所以忽略
-  Object.keys(attrs).forEach(key => {
+  Object.keys(attrs).forEach((key) => {
     const matches = key.match(MATCH_EVENT_ATTR);
     if (!matches) return;
     const [g, eventStartKey, eventName] = matches;
     // bind:* , bindtap => onTap
     // catch:* , catchtap => catchTap
-    let evtName = commonEventMapping[eventName]
+    let evtName = commonEventMapping[eventName];
     if (!evtName) {
-      evtName = eventName[0].toUpperCase() + eventName.slice(1)
+      evtName = eventName[0].toUpperCase() + eventName.slice(1);
     }
     // bind => on, 否则默认
-    const attr = eventStartKey.replace('bind', 'on') + evtName;
-    const value = attrs[key]
-    attrs[attr] = value === '' ? true : value
+    const attr = eventStartKey.replace("bind", "on") + evtName;
+    const value = attrs[key];
+    attrs[attr] = value === "" ? true : value;
     if (attr !== key) {
-      delete attrs[key]
+      delete attrs[key];
     }
-  })
+  });
   return node;
 }
 
 function returnSelf(node) {
-  return node
+  return node;
 }
